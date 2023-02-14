8f5d039805740b066937bb1c02568801698ae24d
beshik7 <tommypdx7@gmail.com>
2023-02-14 00:36:32 -0800

    коммит мастер

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+    public static void task1() {
+        System.out.println(" Задача 1 ");
+        byte phone = 70;
+        short screen = 555;
+        int battery = 300000;
+        long dcIn = 50000000;
+        float keyCap = 3.55F;
+        double chargeBrick = 5.9843;
+        System.out.println(" значение переменной phone с типом byte равно " + phone);
+        System.out.println(" значение переменной screen с типом short равно " + screen);
+        System.out.println(" значение переменной battery с типом int равно " + battery);
+        System.out.println(" значение переменной dcIn с типом long равно " + dcIn);
+        System.out.println(" значение переменной keyCap с типом float равно " + keyCap);
+        System.out.println(" значение переменной chargeBrick с типом double равно " + chargeBrick);    }
+
+}
